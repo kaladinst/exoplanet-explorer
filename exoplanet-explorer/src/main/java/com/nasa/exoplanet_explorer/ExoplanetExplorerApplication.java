035112351d433054a751e107acadd22801708b01
emir <emir.d10@outlook.com>
2026-02-12 23:29:42 +0300

    set up the project from the spring initialzr

---
+++ exoplanet-explorer/src/main/java/com/nasa/exoplanet_explorer/ExoplanetExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.nasa.exoplanet_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoplanetExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoplanetExplorerApplication.class, args);
+	}
+
+}
---
+++ exoplanet-explorer/src/test/java/com/nasa/exoplanet_explorer/ExoplanetExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa.exoplanet_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoplanetExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
